bfdb5d59874bf58866ba6b48f6f4c1a3894b3b99
Alexander Al <web-alexandr-akk@mail.ru>
2025-11-18 20:49:16 +0300

    Создал пустой проект

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+// import { useState } from 'react';
+import styles from './app.module.css';
+
+export const App = () => {
+	return (
+		<div className={styles.app}>
+			<h1>Инструкция по готовке пельменей</h1>
+		</div>
+	);
+};
